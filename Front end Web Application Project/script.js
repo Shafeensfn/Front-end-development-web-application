a15cf634c5c9f8b35034c447994ae14ada7e774f
Shafeen <47037607+Shafeensfn@users.noreply.github.com>
2023-12-18 02:30:03 +0530

    Add files via upload

---
+++ Front end Web Application Project/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var clickMeBtn = document.getElementById('clickMeBtn');
+
+    clickMeBtn.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+});
